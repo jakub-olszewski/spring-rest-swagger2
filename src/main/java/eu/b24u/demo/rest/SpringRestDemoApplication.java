b700b1819c8b00f4c4e24de8e1705f4dac45fb29
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-06-21 21:18:15 +0200

    repair deploy wildfly

--- src/main/java/eu/b24u/demo/rest/SpringRestDemoApplication.java
+++ src/main/java/eu/b24u/demo/rest/SpringRestDemoApplication.java
@@ -2,10 +2,11 @@ package eu.b24u.demo.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-
+@ComponentScan
 @SpringBootApplication
-public class SpringRestDemoApplication {
+public class SpringRestDemoApplication {// extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestDemoApplication.class, args);
